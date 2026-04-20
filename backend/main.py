9448728cd53bd78649facf81ec77202be2e9dbf2
manu093 <manu0093@gmail.com>
2026-04-20 15:35:52 -0300

    Add logging to optimizer to debug chain filter

--- backend/main.py
+++ backend/main.py
@@ -1771,6 +1771,7 @@ def catalog_optimizar(body: dict):
     tarjetas_sel = body.get("tarjetas_seleccionadas", {})
     dia = body.get("dia_semana", "")
     cadenas_filter = body.get("cadenas", [])
+    import logging; logging.info(f"OPTIMIZE: cadenas_filter={cadenas_filter}, dia={dia}")
 
     cn = _pg(); cr = cn.cursor()
 
