2ce327280091aff544d9f4fcc9004caa45f056f2
manu093 <manu0093@gmail.com>
2026-05-29 12:25:07 -0300

    fix: build.gradle tenia version 1.5.5 en vez de 1.6.0

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -15,8 +15,8 @@ android {
         applicationId = "com.sharedshoppinglists.app"
         minSdk = 26
         targetSdk = 35
-        versionCode = 13
-        versionName = "1.5.5"
+        versionCode = 14
+        versionName = "1.6.0"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
