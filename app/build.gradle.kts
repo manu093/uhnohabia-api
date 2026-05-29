4290b438d377a1dd2aa251e0425720553f9cd094
manu093 <manu0093@gmail.com>
2026-05-29 00:04:14 -0300

    v1.4.1: Test OTA

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -15,8 +15,8 @@ android {
         applicationId = "com.sharedshoppinglists.app"
         minSdk = 26
         targetSdk = 35
-        versionCode = 4
-        versionName = "1.4.0"
+        versionCode = 5
+        versionName = "1.4.1"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
